15be45d09f24668ab7e24658832ef82dbe4bda13
nasimudheen-t <comnasim756@gmail.com>
2026-05-15 09:06:34 +0530

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cream-bg': '#000000',
+        'cream-alt': '#0a0a0a',
+        'border-soft': '#333333',
+        'text-deep': '#ffffff',
+        'text-muted': '#a3a3a3',
+        'accent-gold': '#ef4444',
+      }
+    },
   },
   plugins: [],
 }
