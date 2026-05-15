7dbf9dd2658c190ff8ef50bc2ce4429c8172707e
nasimudheen-t <comnasim756@gmail.com>
2026-05-15 18:49:52 +0530

    Added Microsoft Clarity setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
+
+import clarity from "@microsoft/clarity";
+
+clarity.init("wri9289fbu");
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
